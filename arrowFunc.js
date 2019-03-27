6e68fc15d7792b1c854004852b537a9765cd01fc
TruongNM <manhtruonghedspi@gmail.com>
2019-03-28 07:41:12 +0900

    arrow func

---
+++ arrowFunc.js
@@ -0,0 +1,27 @@
+function printMyName(name) {
+    console.log(name);
+}
+printMyName('Max');
+
+
+
+
+
+
+const printMyName = name => {
+    console.log(name);
+}
+printMyName('Max');
+
+const printMyName = (name) => {
+    console.log(name);
+}
+printMyName('Max');
+
+const multiply = (number) => number * 2;
+console.log(multiply(2))
+
+const multiply = (number) => {
+    return number * 2;
+};
+console.log(multiply(2))
