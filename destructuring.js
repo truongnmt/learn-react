9e8fee3daa04dc71cad1233d16af5f955c8bab9d
TruongNM <manhtruonghedspi@gmail.com>
2019-03-30 17:42:07 +0900

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3];
+[num1, num2] = numbers;
+console.log(num1, num2);
+// 1, 2
+
+{name} = {name: 'Max', age: 28}
+console.log(name) // Max
+console.age(age) // undefined
+
+{name, age} = {name: 'Max', age: 28}
+console.log(name) // Max
+console.age(age) // 28
